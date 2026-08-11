3c93c8bf95dc0c678e13ac894c40bc3a567cc349
Faissal Mahmoud <149112005+faissalelmasry@users.noreply.github.com>
2026-08-11 22:53:48 +0300

    changing environment

--- Koralytics.Angular/src/environments/environment.ts
+++ Koralytics.Angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.koralytics.com', // Placeholder for prod
+  apiUrl: 'https://koralytics-d7apdcejccgqcgcv.centralus-01.azurewebsites.net',
   googleClientId: '1058453634827-mbmccfc7ema4ilrofjnrijor8cckgem1.apps.googleusercontent.com'
 };
