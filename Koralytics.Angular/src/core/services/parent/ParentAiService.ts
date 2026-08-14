559bfac3dd5022e331b78a080f2d1621d49645bd
Faissal Mahmoud <149112005+faissalelmasry@users.noreply.github.com>
2026-08-15 00:27:40 +0300

    Fixing parent ai environment

--- Koralytics.Angular/src/core/services/parent/ParentAiService.ts
+++ Koralytics.Angular/src/core/services/parent/ParentAiService.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 export interface ParentChatRequest {
   sessionId: string;
